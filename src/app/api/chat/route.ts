4df993d546d1f7aff7e1542874a19bb1f3b4f855
rmonteroh <rmonteroh90@gmail.com>
2023-10-13 14:19:24 -0500

    revert

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -14,7 +14,7 @@ export async function POST(request: NextRequest) {
     const body = await request.json();
     const datasource = new DataSource({
       type: "sqlite",
-      database: "./Contructions.db",
+      database: "./public/Contructions.db",
     });
 
     const db = await SqlDatabase.fromDataSourceParams({
